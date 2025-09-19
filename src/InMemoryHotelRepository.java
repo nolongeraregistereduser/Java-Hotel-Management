9c3da4cd7bad76c4031fbf50e152a8679ba494af
nolongeraregistereduser <mohamedzouhairi023@gmail.com>
2025-09-19 11:14:31 +0100

    adding hotels hard coded for testing

--- src/InMemoryHotelRepository.java
+++ src/InMemoryHotelRepository.java
@@ -7,6 +7,15 @@ import java.util.UUID;
 public class InMemoryHotelRepository implements HotelRepository {
     private final Map<UUID, Hotel> store = new HashMap<>();
 
+    public InMemoryHotelRepository() {
+        // Hardcoded Moroccan hotels for testing
+        save(new Hotel(UUID.randomUUID(), "La Mamounia", 10, 4.8));
+        save(new Hotel(UUID.randomUUID(), "Royal Mansour", 5, 4.9));
+        save(new Hotel(UUID.randomUUID(), "Four Seasons Resort Marrakech", 8, 4.7));
+        save(new Hotel(UUID.randomUUID(), "Sofitel Casablanca Tour Blanche", 12, 4.5));
+        save(new Hotel(UUID.randomUUID(), "Kenzi Tower Hotel", 7, 4.3));
+    }
+
     @Override
     public void save(Hotel hotel) {
         store.put(hotel.getHotelId(), hotel);
@@ -42,4 +51,6 @@ public class InMemoryHotelRepository implements HotelRepository {
     public void delete(UUID hotelId) {
         store.remove(hotelId);
     }
+
+
 }
